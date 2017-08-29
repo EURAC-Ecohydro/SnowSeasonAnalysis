35ee2c60eaced6bc342a3a46c11228331eb603bc
bridachristian <bridachristian@gmail.com>
2017-08-29 16:05:14 +0200

    Update method of calculation SWE from snow height signal

--- R/SWE_estimation.R
+++ R/SWE_estimation.R
@@ -12,18 +12,31 @@ NULL
 #' @param AIR_TEMPERATURE names of column in input data corresponding with air temperature parameter
 #' @param WIND_SPPED names of column in input data corresponding with wind speed parameter
 
+# # Input
+# git_folder="C:/Users/CBrida/Desktop/Git/EURAC-Ecohydro/SnowSeasonAnalysis/"
+# 
+# PATH <- paste(git_folder,"/data/Input_data/",sep = "")      # <-- path of original dataset 
+# FILE  <- "B3_2000m_TOTAL.csv" # <-- with .csv    # <--  name of file of original dataset 
+# 
+# PATH_SNOW <- paste(git_folder,"/data/Output/Snow_Filtering/",sep = "")      # <-- path of snow dataset elaborated from "Filtering_snow_height.R" algorithms
+# FILE_SNOW <- paste("Snow_",FILE,sep = "")        # <--  name of file of snow dataset 
+# 
+# PATH_ESOLIP_QC <- paste(git_folder,"/data/Output/Precipitation_metadata/ESOLIP_QC_Steps/",sep = "")      # <-- path of snow dataset elaborated from "Filtering_snow_height.R" algorithms
+# FILE_ESOLIP_QC <- paste("ESQC_Steps_",FILE,sep = "")        # <--  name of file of snow dataset 
+# 
+# PRECIPITATION <- "Precip_T_Int15"    # <-- assign here the column name corresponding with precipitation parameter. Default (LTER stations) is "Precip_T_Int15" 
+# AIR_TEMPERATURE <- "T_Air"          # <-- assign here the column name corresponding with air temperature parameter. Default (LTER stations) is "T_Air" 
+# WIND_SPPED <- "Wind_Speed"          # <-- assign here the column name corresponding with wind speed parameter. Default (LTER stations) is "Wind_Speed" 
+# 
 
-SWE_estimation=function(PATH, FILE, PATH_SNOW, FILE_SNOW,  git_folder, 
-                        PRECIPITATION = "Precip_T_Int15", AIR_TEMPERATURE = "T_Air", WIND_SPPED = "Wind_Speed"){
+
+SWE_estimation=function(git_folder,PATH, FILE,
+                        PATH_SNOW, FILE_SNOW,
+                        PATH_ESOLIP_QC, FILE_ESOLIP_QC,   
+                        PRECIPITATION = "Precip_T_Int15", AIR_TEMPERATURE = "T_Air", WIND_SPPED = "Wind_Speed",
+                        INCREMENT_THRESHOLD = 0.001 ){
   
-  PATH = path
-  FILE = file
-  
-  PATH_SNOW = path_filtered_snow
-  FILE_SNOW = file_snow
-  
-  # PATH_PHASE = path_phase
-  # FILE_PHASE = file_phase
+  # INCREMENT_THRESHOLD = 0.001  # <-- threshold on snow height increment (increment less then 0.002 mm/h of snow) 
   
   source(paste(git_folder,"/R/esqc_Read_data_metadata.R",sep=""))
   
@@ -34,30 +47,38 @@ SWE_estimation=function(PATH, FILE, PATH_SNOW, FILE_SNOW,  git_folder,
   
   T_Kelvin=air_temperature_original+273.15
   
+  T_Kelvin2=T_Kelvin
+  T_Kelvin2[which(is.na(T_Kelvin))] = 273.15
+
+  wind=wind_original
+  wind[is.na(wind_original)] = 1
+  
   rho = rep(NA, times=length(air_temperature_original))
   rho = zoo(rho,order.by = index(air_temperature_original))
   
-  g1= which(T_Kelvin>275.65)
-  rho[g1]=NA
+  g0 = which(T_Kelvin2 > 283.15)
+  rho[g0]=NA 
   
-  g2 = which(T_Kelvin>260.15 & T_Kelvin <=275.65)
-  rho[g2]=500*(1-0.951*exp(-1.4*(278.15-T_Kelvin[g2])^(-1.15))-0.008*wind_original[g2]^1.7)
+  g1= which(T_Kelvin2>275.65 & T_Kelvin2 <=283.15)
+  rho[g1]=200
   
-  g3 = which(T_Kelvin <= 260.15)
-  rho[g3] = 500*(1-0904*exp(-0.008*wind_original[g3]^1.7))
+  g2 = which(T_Kelvin2>260.15 & T_Kelvin2 <=275.65)
+  rho[g2]=500*(1-0.951*exp(-1.4*(278.15-T_Kelvin2[g2])^(-1.15))-0.008*wind[g2]^1.7)
+  
+  g3 = which(T_Kelvin2 <= 260.15)
+  rho[g3] = 500*(1-0904*exp(-0.008*wind[g3]^1.7))
   
   rho2=rho
-  rho2[which(rho<(-100))]=NA
+  rho2[which(rho<(0))]=NA
   
   zoo_snow=fun_read_output_data(PATH_SNOW,FILE_SNOW)
   snow=zoo_snow[,which(colnames(zoo_snow)=="HS_calibr_smooothed_rate_QC")]  
   
-
   
   num_snow = as.numeric(snow)
   deriv=diff(num_snow)
   accum=deriv
-  w=which(deriv<0.0025)
+  w=which(deriv<INCREMENT_THRESHOLD)
   accum[w] = NA
   accum=c(NA,accum)
   
@@ -67,11 +88,27 @@ SWE_estimation=function(PATH, FILE, PATH_SNOW, FILE_SNOW,  git_folder,
   
   SWE=accum*rho_accum
   
+
+  esolip_metadata=fun_read_output_metadata(PATH_ESOLIP_QC,FILE_ESOLIP_QC)
+  phase=esolip_metadata[,which(colnames(esolip_metadata)=="STEP2")]
+  classification=esolip_metadata[,which(colnames(esolip_metadata)=="STEP6")]
+  
+  SWE_out=SWE
+  SWE_out[which(phase=="Rain")]=NA
+
   derivat_snow=zoo(c(NA,deriv),order.by = index(precip_original))
-  output=list(SWE,snow,derivat_snow,precip_original)
-  names(output)=c("SWE","snow","derivate_snow","precipitation")
+  output=list(SWE, SWE_out,snow,derivat_snow,precip_original,rho2)
+  names(output)=c("SWE","SWE_filtered","snow","derivate_snow","precipitation", "rho")
   
   return(output)
-
+  
 }
+
+
+
+
+
+
+
+
 
